a8385d4d8f9781fba351215fddcb506f138deb77
neya-mul <neyamulislam946@gmail.com>
2026-03-30 11:42:04 +0600

    card is ready now

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -18,7 +18,7 @@ export default function CardContainer({items}) {
                 </div>
             </div>
 
-            <div>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     data.map(card=> <Cards key={card.id} card={card}></Cards>)
                 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,15 +4,17 @@ import { MdVerified } from 'react-icons/md'
 export default function Cards({ card }) {
   console.log(card)
   return (
-    <div className='border p-10 max-w-[500px]'>
+    <div className='border border-gray-300 rounded-2xl p-10 max-w-[500px] mx-auto space-y-4 '>
 
       <div className='flex justify-between'>
 
         <img src={card.image} alt="" className='h-[50px] w-fit' />
-        <span className='border'>{card.tag}</span>
+        <span className=
+        {`${card.tag === 'new' ? 'bg-green-400 text-green-800' : card.tag === 'popular' ? 'bg-purple-400 text-purple-800' : card.tag=== 'best seller' ? 'bg-yellow-400 text-yellow-700': ''} rounded-2xl p-2`}
+        >{card.tag}</span>
       </div>
-      <h1>{card.name}</h1>
-      <p>{card.description}</p>
+      <h1 className='text-3xl'>{card.name}</h1>
+      <p className='min-h-15'>{card.description}</p>
       <p>{card.price}/month</p>
       <p className='flex items-center'> <span className='text-green-500'><MdVerified /></span> {card.features[0]}</p>
       <p className='flex items-center'> <span className='text-green-500'><MdVerified /></span> {card.features[1]}</p>
