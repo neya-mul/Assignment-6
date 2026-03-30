070e939ac312b13a8cce300c80adf01e4652f569
neya-mul <neyamulislam946@gmail.com>
2026-03-30 07:47:11 +0600

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Neyamul</h1>
-      <div className="navbar bg-base-100 shadow-sm">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
+    <Navbar></Navbar>
+      
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+
+import { FaCartShopping } from 'react-icons/fa6'
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {/* <li><a>Item 1</a></li> */}
+                        <li><a>Products</a></li>
+                        <li><a>Features</a></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Testimonials</a></li>
+                        <li><a>FAQ</a></li>
+                        {/* <li><a>Item 3</a></li> */}
+                    </ul>
+                </div>
+                <a className="text-2xl font-bold bg-linear-to-r from-blue-600 to-purple-500 bg-clip-text text-transparent">DigiTools</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Products</a></li>
+                    <li><a>Features</a></li>
+                    <li><a>Pricing</a></li>
+                    <li><a>Testimonials</a></li>
+                    <li><a>FAQ</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end gap-3">
+                <button className='flex items-center'><FaCartShopping />
+                    <span>0</span> </button>
+                <button>Login</button>
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    )
+}
