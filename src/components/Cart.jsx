c7deb96418340604cf5a214eb1b8def84b5ed7af
neya-mul <neyamulislam946@gmail.com>
2026-03-31 09:23:59 +0600

    delete button functionality added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import CartCom from './CartCom'
+import { toast } from 'react-toastify'
 
 export default function Cart({ isBuyed, setIsBuyed }) {
+    isBuyed.map(el => el.name)
+    // console.log(el)
+
     let totalPrice = isBuyed.reduce((sum, item) => sum + item.price, 0)
-    const proceed = ()=>{
+
+    const proceed = () => {
         setIsBuyed([])
+        toast('All the items had prceded to checkout')
     }
 
     // console.log(totalPrice)
@@ -12,21 +18,21 @@ export default function Cart({ isBuyed, setIsBuyed }) {
         <div className='w-full max-w-[1400px] mx-auto col-span-full space-y-2 border p-5 border-gray-400 rounded-2xl'>
             <h1 className='text-3xl'>Your Cart</h1>
 
-         {isBuyed.length > 0?   <div className='cards space-y-3'>
+            {isBuyed.length > 0 ? <div className='cards space-y-3'>
 
                 {
-                    isBuyed.map(product => <CartCom product={product}></CartCom>)
+                    isBuyed.map(product => <CartCom product={product} isBuyed={isBuyed} setIsBuyed={setIsBuyed}></CartCom>)
                 }
             </div>
-            : <div className='text-center'>
-                <h1 className='text-2xl'>Your cart is empty</h1>
-                <p>Please select any item</p>
-            </div>
+                : <div className='text-center'>
+                    <h1 className='text-2xl'>Your cart is empty</h1>
+                    <p>Please select any item</p>
+                </div>
             }
 
             <div className='flex justify-between'>
-                <span>Price</span>
-                <span>{totalPrice}</span>
+                <span className='text-2xl'>Price</span>
+                <span className='font-bold text-2xl'>{totalPrice}</span>
             </div>
             <div>
                 <button onClick={proceed} className='btn btn-primary rounded-2xl w-full'>Proceed to Checkout</button>
--- src/components/CartCom.jsx
+++ src/components/CartCom.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 
-export default function CartCom({ product }) {
+export default function CartCom({ product, isBuyed, setIsBuyed }) {
+  const deleteButton= (product)=>{
+    // console.log(product);
+    const filteredProduct = isBuyed.filter(p=> p.name != product.name)
+    // console.log(filteredProduct)
+    setIsBuyed(filteredProduct)
+    
+  }
   return (
     <div className='space-y-3.5'>
       {/* cards */}
       <div className='flex justify-between p-2 items-center border border-gray-300 rounded-2xl'>
         <div className='flex items-center gap-3'>
-          <img src={product.image} className='h-[50px] w-fit' alt="" />
+          <img src={product.image} className='h-fit w-[70px]' alt="" />
           <div className=''>
-            <h1>{product.name}</h1>
-            <p className='items-center'>${product.price}</p>
+            <h1 className='text-xl'>{product.name}</h1>
+            <p className='items-center font-bold'>${product.price}</p>
           </div>
 
         </div>
-        <button className='btn text-red-500'>Remove</button>
+        <button className='btn text-red-500' onClick={()=>deleteButton (product)}>Remove</button>
       </div>
 
     </div>
