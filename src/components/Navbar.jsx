da020d9d091c27163dd414caee515939d6fdbbd0
neya-mul <neyamulislam946@gmail.com>
2026-04-01 10:16:10 +0600

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import cart from "../images/products/shopping-cart.png"
 import { FaCartShopping } from 'react-icons/fa6'
 
-export default function Navbar({isAdded, setIsAdded}) {
-  
-    
+export default function Navbar({ isAdded, setIsAdded }) {
+
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -20,6 +20,7 @@ export default function Navbar({isAdded, setIsAdded}) {
                         <li><a>Pricing</a></li>
                         <li><a>Testimonials</a></li>
                         <li><a>FAQ</a></li>
+                        <button className="btn">Login</button>
                         {/* <li><a>Item 3</a></li> */}
                     </ul>
                 </div>
@@ -36,8 +37,8 @@ export default function Navbar({isAdded, setIsAdded}) {
             </div>
             <div className="navbar-end gap-3">
                 <button className='flex items-center'><img src={cart} alt="" />
-                    <span className={`mb-6 ${isAdded.length>0 ? 'bg-red-400' : ''} p-1 rounded-full text-sm text-white`}>{isAdded.length>0? `${isAdded.length}` : null}</span> </button>
-                <button>Login</button>
+                    <span className={`mb-6 ${isAdded.length > 0 ? 'bg-red-400' : ''} p-1 rounded-full text-sm text-white`}>{isAdded.length > 0 ? `${isAdded.length}` : null}</span> </button>
+                <button className="hidden lg:block">Login</button> 
                 <a className="btn bg-linear-to-r from-blue-600 to-purple-500 text-white rounded-4xl">Get Started</a>
             </div>
         </div>
